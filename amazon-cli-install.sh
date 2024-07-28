e53aca4dc075db9a928c86bb1bdaafdf5d501b77
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-28 18:41:21 -0400

    Started working amazon cli install.

--- amazon-cli-install.sh
+++ amazon-cli-install.sh
@@ -6,10 +6,20 @@
 sudo apt update
 
 # Install Python and pip if not already installed
-sudo apt install -y python3 python3-pip
+if ( which python)
+then
+  echo "python already installed"
+else
+  echo "Installing python, and pip"
+  sudo apt install -y python3 python3-pip
 
 # Install the AWS CLI using pip
-pip3 install awscli --upgrade --user
+if ()
+then
+  echo " AWS cli already installed"
+else
+  echo "Installing AWS cli"
+  pip3 install awscli --upgrade --user
 
 # Add the AWS CLI executable to the system path
 echo "export PATH=\$PATH:~/.local/bin" >> ~/.bashrc
