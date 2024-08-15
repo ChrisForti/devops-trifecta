d5c75d85a0260a9635a34299538c84dfc200fca7
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-14 20:00:58 -0400

    Updated delete vm to exclude testing.

--- destroy-vm.sh
+++ destroy-vm.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
 # delete and purge trifecta VM
-if ( ! multipass list | grep trifecta )
-then
-  echo "no trifecta VM found"
-else 
-  multipass delete --purge trifecta
-  echo "trifecta VM has been deleted and purged"
-fi
-
-if (stat id_ed25519)
-then
-  echo "removing ssh key"
-  rm -f id_ed25519*
-else
-  echo "ssh keys do not exist"  
-fi
-
-if (stat cloud-init.yaml)
-then
-  echo "Removing cloud-init.yaml"
-  rm cloud-init.yaml
-  echo "Cloud-init.yaml removed."
-else
-  echo "No cloud-init.yaml file detected."
-fi
+multipass delete --purge trifecta
+# Remove any and all id_ed255129 ssh keys 
+rm -f id_ed25519*
+# Remove cloud-init yaml file
+rm cloud-init.yaml
